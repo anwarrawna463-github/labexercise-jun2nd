e7d344913da92531c59bfee0739d1c259044970b
anwarrawna463-github <79622763+anwarrawna463-github@users.noreply.github.com>
2021-06-03 19:33:31 +0530

    Add files via upload

---
+++ QuotientAndReminder.java
@@ -0,0 +1,11 @@
+package lab_exercise_1;
+
+public class QuotientAndReminder {
+public static void main(String[] args) {
+	int number1=50,number2=10;
+	int quotient=number1/number2;
+	int reminder=number1%number2;
+	System.out.println("quotient is: "+quotient);
+	System.out.println("the remnder is:"+reminder);
+}
+}
