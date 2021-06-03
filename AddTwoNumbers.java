eebe488f415b5835c214dbfbca5958a6c71d6d60
anwarrawna463-github <79622763+anwarrawna463-github@users.noreply.github.com>
2021-06-03 19:28:17 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package lab_exercise_1;
+
+public class AddTwoNumbers {
+public static void main(String[] args) {
+	int number1=150,number2=500,sum;
+	sum=number1+number2;
+	System.out.println("the sum is: "+sum);
+}
+}
